94b65d75975af2e269cb95115da9eb005596ae75
Mohamadka1 <124268603+Mohamadka1@users.noreply.github.com>
2024-11-21 13:54:51 +0100

    huis mig

--- migrations/20241108082626_create_houses_table.js
+++ migrations/20241108082626_create_houses_table.js
@@ -5,8 +5,12 @@
 exports.up = function(knex) {
     return knex.schema.createTable('houses', (table) => {
       table.increments('id').primary(); // Auto-incrementing primary key
+      table.string('title').notNullable(); // Name of the house
+      table.string('city').notNullable(); // Name of the hous
+      table.string('price').notNullable(); // Name of the house
       table.string('name').notNullable(); // Name of the house
-      table.string('address').notNullable(); // Address of the house
+      table.string('post-cod').notNullable(); // Address of the house
+      table.string('street').notNullable(); // Address of the house
       table.timestamps(true, true); // created_at and updated_at columns
     });
   };
