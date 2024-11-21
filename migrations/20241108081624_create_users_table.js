29cf0be07cb6a6b07abed9a2ea87c2826df17ddf
Nawaf1w3 <57842047+Nawaf1w3@users.noreply.github.com>
2024-11-21 13:35:37 +0100

    ss

--- migrations/20241108081624_create_users_table.js
+++ migrations/20241108081624_create_users_table.js
@@ -6,6 +6,13 @@ exports.up = function(knex) {
     return knex.schema.createTable('users', (table) => {
         table.increments('id').primary();
         table.string('name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('gender').notNullable();
+        table.date('birth_date').notNullable();
+        table.string('current_adress').notNullable();
+        table.string('phone').notNullable();
+        table.string('income').notNullable();
+        table.string('state').notNullable();
         table.string('email').unique().notNullable();
         table.enu('role', ['admin', 'customer']).notNullable().defaultTo('customer'); // Add role column
         table.enu('user_type', ['inwoners', 'wonningzoekende']).notNullable().defaultTo('inwoners'); // Add user_type column
